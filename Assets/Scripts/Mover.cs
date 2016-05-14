d1575c49c6014cdcc4c372f3f3045ad80153f7c4
Erdboden <mark.ghelas@gmail.com>
2016-05-14 19:38:20 +0300

    add functionality
    gamecontroller, update destroyByContact script, mover, playerController
    and destroyByTime scripts.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody myRigidbody;
+
+    
+    void Start()
+    {
+        this.myRigidbody = this.GetComponent<Rigidbody>();
+        this.myRigidbody.velocity = transform.forward * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject engineLeft;
+    public GameObject engineRight;
+
+    private GameController gameController;
+    private Rigidbody myRigidbody;
+    private void Start()
+    {
+        this.myRigidbody = this.GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+      
+        engineRight.SetActive(false);
+        engineLeft.SetActive(false);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+ 
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        this.myRigidbody.velocity = movement * speed;
+
+        this.myRigidbody.position = new Vector3
+                (
+                    Mathf.Clamp(this.myRigidbody.position.x, boundary.xMin, boundary.xMax),
+                    1f,
+                    -8f
+                );
+        if(Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
+            engineRight.SetActive(true);
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+            engineLeft.SetActive(true);
+        /*if (this.myRigidbody.position.x<0)
+        {
+            // this.myRigidbody.rotation = Quaternion.Euler(this.myRigidbody.velocity.x * -tilt,90f , 90f);
+            engineRight.SetActive(true);
+
+        }
+        if (this.myRigidbody.position.x>0)
+        {
+            //this.myRigidbody.rotation = Quaternion.Euler(this.myRigidbody.velocity.x * -tilt, -90f, -90f);
+            engineLeft.SetActive(true);
+        }*/
+       
+
+    }
+}
